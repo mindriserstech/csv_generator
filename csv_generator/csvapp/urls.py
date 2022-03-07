6df0619993482b21029c137b91779db0f102e265
sixdgt <c4crypt@gmail.com>
2022-03-07 13:48:54 +0545

    url and view

--- csv_generator/csvapp/urls.py
+++ csv_generator/csvapp/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-import views
+from . import views
 urlpatterns = [
     # this path() method takes three parameter
     # 1. url i.e '/usertypes/'
     # 2. views.<function_name> i.e views.usertype_index
     # 3. name="<unique_route_name>" i.e name="usertype.name"
-    path('/usertypes/', views.usertype_index, name="usertype.index"),
-    path('/usertypes/create/', views.usertype_create, name="usertype.create"),
-    path('/usertypes/store/', views.usertype_store, name="usertype.store"),
-    path('/usertypes/udpate/', views.usertype_update, name="usertype.udpate"),
-    path('/usertypes/edit/<int:id>', views.usertype_edit, name="usertype.edit"),
-    path('/usertypes/show/<int:id>', views.usertype_show, name="usertype.show"),
-    path('/usertypes/delete/<int:id>', views.usertype_delete, name="usertype.delete"),
+    path('usertypes/', views.usertype_index, name="usertype.index"),
+    path('usertypes/create/', views.usertype_create, name="usertype.create"),
+    path('usertypes/store/', views.usertype_store, name="usertype.store"),
+    path('usertypes/udpate/', views.usertype_update, name="usertype.udpate"),
+    path('usertypes/edit/<int:id>', views.usertype_edit, name="usertype.edit"),
+    path('usertypes/show/<int:id>', views.usertype_show, name="usertype.show"),
+    path('usertypes/delete/<int:id>', views.usertype_delete, name="usertype.delete"),
 
     # user
     # path('/users/', views.usertype_index, name="users.index"),
--- csv_generator/csvapp/views.py
+++ csv_generator/csvapp/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 # user type view function
 
 def usertype_index(request):
     ''' this function displays all the data of usertype '''
-    return render(request)
+    template = 'usertypes/index.html'
+    content = {
+        'title': "Usertype List",
+        'description': {
+            'id': 1,
+            'usertype': 'UserAdmin',
+            'status': 'active'
+        }, 
+        'num': range(10)
+    }
+    return render(request, template, content)
 
 def usertype_create(request):
     ''' this function create form for usertype '''
-    return render(request)
+    template = 'usertypes/create.html'
+    content = {
+        'title': "Mind Risers | Django Class",
+        'description': 'Poush Session Evening Group',
+        'total_students': '10 Students in total'
+    }
+    return render(request, template, content)
 
 def usertype_store(request):
     ''' this function stores all the data of usertype '''
